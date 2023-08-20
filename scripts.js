562219507ea8a1117bd28c6d5f638276deb8701e
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-20 12:16:10 +0200

    scroll project working correctly

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const boxes = document.querySelectorAll(".hide")
 window.onscroll = (() => {
     boxes.forEach((box) => {
         const info = box.getBoundingClientRect()
-        if(info.top <= 800){
+        if(info.top <= 700){
             box.classList.remove("hide")
         }else{
             box.classList.add("hide")
